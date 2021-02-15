14df6bdcaee9b52abd01294c0c71c62c6c4b0356
InduLekha98 <rindulekha09@gmail.com>
2021-02-15 11:40:31 +0530

    Added:Welcome file

--- employee.java
+++ employee.java
@@ -1,5 +1,6 @@
 public class employee {
  public static void main(String[] args) {
- System.out.println("Welcome to employee program");
+   System.out.println("Welcome to employee program");
+   System.out.println("Welcome");
  }
 }
